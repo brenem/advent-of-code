ca09ccc03f5f4cbe1629ebd0177104c7a7492c78
Brett Nemec <128428098+brenem@users.noreply.github.com>
2025-12-05 14:42:25 -0500

    updating day20.ts

--- src/challenges/2024/day20.ts
+++ src/challenges/2024/day20.ts
@@ -1,14 +1,7 @@
-import { manhattan } from '../../algorithms/astar/heuristics';
-import { groupBy } from '../../helpers/utils';
 import { Grid, GridCoordinate } from '../../models/grid';
 import { Point } from '../../models/point';
 import { PuzzleResult } from '../../models/puzzle-result';
 import { Challenge } from '../challenge';
-
-interface CheatCost {
-    cheatLocation: string;
-    saving: number;
-}
 
 export class Day20 extends Challenge {
     async part1(input: string): Promise<PuzzleResult> {
@@ -16,12 +9,10 @@ export class Day20 extends Challenge {
         const grid = new Grid(map);
 
         const tracks = this.getTracks(grid);
+        const cheatPaths = this.findCheatPaths(grid, tracks);
 
-        const cheatLimit = 2;
-        const cheatPaths = this.findCheatPaths(grid, tracks, cheatLimit, cheatLimit);
-
-        const savings: CheatCost[] = cheatPaths.map((c) => ({
-            cheatLocation: `${c.cheatLocation.location.x},${c.cheatLocation.location.y}`,
+        const savings = cheatPaths.map((c) => ({
+            cheatLocation: c.cheatLocation,
             saving: tracks.length - c.tracksCount
         }));
 
@@ -29,30 +20,7 @@ export class Day20 extends Challenge {
     }
 
     async part2(input: string): Promise<PuzzleResult> {
-        const map = this.dataLoader.readLines(input, (line) => line.split(''));
-        const grid = new Grid(map);
-
-        const tracks = this.getTracks(grid);
-
-        const cheatLimit = 20;
-        const cheatPaths = this.findCheatPaths(grid, tracks, 2, cheatLimit);
-
-        const savings: CheatCost[] = cheatPaths.map((c) => ({
-            cheatLocation: `${c.cheatLocation.location.x},${c.cheatLocation.location.y}`,
-            saving: tracks.length - c.tracksCount
-        }));
-
-        const maxSavings = savings.filter((x) => x.saving >= 50);
-
-        for (const maxSaving of maxSavings) {
-            grid.set(+maxSaving.cheatLocation.split(',')[0], +maxSaving.cheatLocation.split(',')[1], 'C');
-        }
-
-        const grouped = groupBy(maxSavings, 'saving');
-
-        this.logger.debug(grid.toString());
-
-        return maxSavings.length;
+        return 0;
     }
 
     private getTracks(grid: Grid): GridCoordinate[] {
@@ -65,6 +33,30 @@ export class Day20 extends Challenge {
             tracks.push(next);
 
             if (next.character === 'E') {
+                break;
+            }
+
+            current = next;
+        }
+
+        return tracks;
+    }
+
+    private getTracksWithSteps(grid: Grid): {step: number, track: GridCoordinate}[] {
+        const start = grid.find('S')!;
+        let current = {step: 0, track: start};
+        const tracks: {step: number, track: GridCoordinate}[] = [current];
+        const visited = new Set<string>([`${current.track.location.x},${current.track.location.y}`]);
+        let step = 0;
+
+        while (true) {
+            step++;
+
+            const nextCell = this.getNextCell(grid, current.track, visited);
+            const next = {step, track: nextCell};
+            tracks.push(next);
+
+            if (next.track.character === 'E') {
                 break;
             }
 
@@ -98,43 +90,43 @@ export class Day20 extends Challenge {
         }
     }
 
-    // private getCheatCells(grid: Grid, cell: GridCoordinate): GridCoordinate[] {
-    //     const eastCell = grid.get(cell.east.x, cell.east.y);
-    //     const westCell = grid.get(cell.west.x, cell.west.y);
-    //     const northCell = grid.get(cell.north.x, cell.north.y);
-    //     const southCell = grid.get(cell.south.x, cell.south.y);
-    //     const cheatNeighbors: GridCoordinate[] = [];
+    private getCheatCells(grid: Grid, cell: GridCoordinate): GridCoordinate[] {
+        const eastCell = grid.get(cell.east.x, cell.east.y);
+        const westCell = grid.get(cell.west.x, cell.west.y);
+        const northCell = grid.get(cell.north.x, cell.north.y);
+        const southCell = grid.get(cell.south.x, cell.south.y);
+        const cheatNeighbors: GridCoordinate[] = [];
 
-    //     if (eastCell && eastCell.character === '#' && !this.isEdge(grid, cell.east)) {
-    //         const eastEastCell = grid.get(eastCell.east.x, eastCell.east.y);
-    //         if (eastEastCell?.character === '.' || eastEastCell?.character === 'E') {
-    //             cheatNeighbors.push(eastEastCell);
-    //         }
-    //     }
+        if (eastCell && eastCell.character === '#' && !this.isEdge(grid, cell.east)) {
+            const eastEastCell = grid.get(eastCell.east.x, eastCell.east.y);
+            if (eastEastCell?.character === '.' || eastEastCell?.character === 'E') {
+                cheatNeighbors.push(eastEastCell);
+            }
+        }
 
-    //     if (westCell && westCell.character === '#' && !this.isEdge(grid, cell.west)) {
-    //         const westWestCell = grid.get(westCell.west.x, westCell.west.y);
-    //         if (westWestCell?.character === '.' || westWestCell?.character === 'E') {
-    //             cheatNeighbors.push(westWestCell);
-    //         }
-    //     }
+        if (westCell && westCell.character === '#' && !this.isEdge(grid, cell.west)) {
+            const westWestCell = grid.get(westCell.west.x, westCell.west.y);
+            if (westWestCell?.character === '.' || westWestCell?.character === 'E') {
+                cheatNeighbors.push(westWestCell);
+            }
+        }
 
-    //     if (northCell && northCell.character === '#' && !this.isEdge(grid, cell.north)) {
-    //         const northNorthCell = grid.get(northCell.north.x, northCell.north.y);
-    //         if (northNorthCell?.character === '.' || northNorthCell?.character === 'E') {
-    //             cheatNeighbors.push(northNorthCell);
-    //         }
-    //     }
+        if (northCell && northCell.character === '#' && !this.isEdge(grid, cell.north)) {
+            const northNorthCell = grid.get(northCell.north.x, northCell.north.y);
+            if (northNorthCell?.character === '.' || northNorthCell?.character === 'E') {
+                cheatNeighbors.push(northNorthCell);
+            }
+        }
 
-    //     if (southCell && southCell.character === '#' && !this.isEdge(grid, cell.south)) {
-    //         const southSouthCell = grid.get(southCell.south.x, southCell.south.y);
-    //         if (southSouthCell?.character === '.' || southSouthCell?.character === 'E') {
-    //             cheatNeighbors.push(southSouthCell);
-    //         }
-    //     }
+        if (southCell && southCell.character === '#' && !this.isEdge(grid, cell.south)) {
+            const southSouthCell = grid.get(southCell.south.x, southCell.south.y);
+            if (southSouthCell?.character === '.' || southSouthCell?.character === 'E') {
+                cheatNeighbors.push(southSouthCell);
+            }
+        }
 
-    //     return cheatNeighbors;
-    // }
+        return cheatNeighbors;
+    }
 
     private isEdge(grid: Grid, cell: Point): boolean {
         return cell.x === 0 || cell.y === 0 || cell.x === grid.width - 1 || cell.y === grid.height - 1;
@@ -142,9 +134,7 @@ export class Day20 extends Challenge {
 
     private findCheatPaths(
         grid: Grid,
-        tracks: GridCoordinate[],
-        cheatLengthMin: number,
-        cheatLengthMax: number
+        tracks: GridCoordinate[]
     ): { cheatLocation: GridCoordinate; tracksCount: number }[] {
         const visited = new Set<string>();
         const cheatPaths: { cheatLocation: GridCoordinate; tracksCount: number }[] = [];
@@ -152,10 +142,7 @@ export class Day20 extends Challenge {
         for (const track of tracks) {
             visited.add(`${track.location.x},${track.location.y}`);
 
-            const cheatLocations = tracks.filter((t) => {
-                const distance = manhattan([track.location.x, track.location.y], [t.location.x, t.location.y]);
-                return distance > 2 && distance <= cheatLengthMax;
-            });
+            const cheatLocations = this.getCheatCells(grid, track);
 
             if (cheatLocations.length > 0) {
                 for (const cheatLocation of cheatLocations) {
@@ -166,7 +153,7 @@ export class Day20 extends Challenge {
                     const cheatIndex = tracks.findIndex(
                         (x) => x.location.x === cheatLocation.location.x && x.location.y === cheatLocation.location.y
                     );
-                    const tracksCountWithCheat = tracks.length - cheatIndex;
+                    const tracksCountWithCheat = tracks.length - cheatIndex + 1;
 
                     cheatPaths.push({
                         cheatLocation: cheatLocation,
